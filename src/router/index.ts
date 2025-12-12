3b401567f647b6e478c9f9da9d42852b9f525a23
junchen <junchen.liu@coolkit.cn>
2025-12-12 16:12:36 +0800

    chore: ⏪️ delete test component and code

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes: [
-        { path: '/', component: () => import('@/views/Home/Index.vue') },
-        { path: '/test', component: () => import('@/views/Test/Index.vue') },
-    ],
+    routes: [{ path: '/', component: () => import('@/views/Home/Index.vue') }],
 });
 
 export default router;
